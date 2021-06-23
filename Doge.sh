1c9ffa6bf1da8f083bbea189c93782a91e2664c1
Rizkycitoz <83176289+Rizkycitoz@users.noreply.github.com>
2021-06-23 21:07:10 +0700

    Create Doge.sh

---
+++ Doge.sh
@@ -0,0 +1,4 @@
+# doge
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig -o stratum+tcp://yescrypt.asia.mine.zergpool.com:3333 -u D6qBwrWUS1MMJRXZW3ycpcX46wa1n7EGQL -p c=DOGE,ID=Tt -p x -k -a rx/0
